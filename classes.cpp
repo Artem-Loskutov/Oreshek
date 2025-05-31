0156d819d78970f5745fbc3b27e217d5b32c797e
Dentera <artem.loskutov@gmail.com>
2025-05-31 19:20:10 +0300

    Ver 0.03

--- classes.cpp
+++ classes.cpp
@@ -10,35 +10,73 @@ sf::Texture load_texture(const std::string& path)
     return texture;
 }
 
-Game::Game(unsigned int window_lengh, unsigned int window_height, const std::string& window_name) :
-    current_game_hour(0), seconds_per_hour(1.0f)
+Game::Game(unsigned int window_width, unsigned int window_height, const std::string& window_name) :
+    game_hour(0), game_day(7), game_month(9), game_year(1941),
+    seconds_per_hour(1.0f)
 {
-    window.create(sf::VideoMode(sf::Vector2u{ window_lengh, window_height }), window_name);
+    window.create(sf::VideoMode(sf::Vector2u{ window_width, window_height }), window_name);
     window.setFramerateLimit(60);
 
-    if (!font.openFromFile("arial.ttf"))
+    if (!font.openFromFile("Assets/arial.ttf"))
     {
         //error
         window.close();
     }
+
+    textures_list["island"] = std::move(load_texture("Assets/island.png"));
 };
 
 void Game::run()
 {
-    sf::Text clock_text(font);
     clock_text.setCharacterSize(16);
-    clock_text.setPosition({ 0.f, 0.f });
-    clock_text.setFillColor(sf::Color::White);
-    
-    sf::Clock clock;
-
-    textures_list["island"] = std::move(load_texture("island.png"));
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << game_hour << ":00 | "
+        << std::setw(2) << std::setfill('0') << game_day << "."
+        << std::setw(2) << std::setfill('0') << game_month << "."
+        << game_year;
+    clock_text.setString(oss.str());
+    sf::FloatRect text_rect = clock_text.getLocalBounds();
+    clock_text.setOrigin({ text_rect.getCenter().x, 0.f});
+    clock_text.setPosition({ static_cast<float>(window.getSize().x / 2), 0.f });
     
     while (window.isOpen())
     {
+        date_maker();
         event_reactions();
+        draw();
+    }
+}
 
-        draw(clock, clock_text);
+void Game::date_maker()
+{
+    if (clock.getElapsedTime().asSeconds() >= seconds_per_hour*speed_mod)
+    {
+        clock.restart();
+        game_hour = game_hour + 1;
+
+        if (game_hour >= 24)
+        {
+            game_hour = 0;
+            game_day++;
+
+            if (game_day > days_in_month[game_month - 1])
+            {
+                game_day = 1;
+                game_month++;
+
+                if (game_month > 12)
+                {
+                    game_month = 1;
+                    game_year++;
+                }
+            }
+        }
+        std::ostringstream oss;
+        oss << std::setw(2) << std::setfill('0') << game_hour << ":00 | "
+            << std::setw(2) << std::setfill('0') << game_day << "."
+            << std::setw(2) << std::setfill('0') << game_month << "."
+            << game_year;
+        clock_text.setString(oss.str());
     }
 }
 
@@ -57,6 +95,18 @@ void Game::event_reactions()
             case (sf::Keyboard::Scancode::Escape):
                 window.close();
                 break;
+            case (sf::Keyboard::Scancode::Num1):
+                speed_mod = 1.0f;
+                break;
+            case (sf::Keyboard::Scancode::Num2):
+                speed_mod = 0.5f;
+                break;
+            case (sf::Keyboard::Scancode::Num3):
+                speed_mod = 0.1f;
+                break;
+            case (sf::Keyboard::Scancode::Num4):
+                speed_mod = 0.01f;
+                break;
             default:
                 break;
             }
@@ -64,21 +114,21 @@ void Game::event_reactions()
     }
 }
 
-void Game::draw(sf::Clock& clock, sf::Text& clock_text)
+void Game::draw()
 {
     window.clear();
 
     sf::Sprite sprite(textures_list["island"]);
-    window.draw(sprite);
-
-    if (clock.getElapsedTime().asSeconds() >= seconds_per_hour)
+    if (game_hour >= 20 || game_hour < 4)
     {
-        clock.restart();
-        current_game_hour = (current_game_hour + 1) % 24;
+        sprite.setColor(sf::Color(128, 128, 128));
     }
-    std::ostringstream oss;
-    oss << std::setw(2) << std::setfill('0') << current_game_hour << ":00";
-    clock_text.setString(oss.str());
+    else
+    {
+        sprite.setColor(sf::Color(255, 255, 255));
+    }
+    window.draw(sprite);
+    
     window.draw(clock_text);
 
     window.display();
--- classes.h
+++ classes.h
@@ -3,22 +3,28 @@
 #include <SFML/Graphics.hpp>
 
 #include <unordered_map>
+#include <vector>
 
 class Game
 {
 private:
     sf::RenderWindow window;
     sf::Font font;
+    sf::Clock clock;
+    sf::Text clock_text {font};
 
     // unique vars
-    int current_game_hour;
+    int game_hour, game_day, game_month, game_year;
     const float seconds_per_hour;
+    float speed_mod = 1.0f;
+    const std::vector<int> days_in_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
     std::unordered_map<std::string, sf::Texture> textures_list;
 
+    void date_maker();
     void event_reactions();
-    void draw(sf::Clock& clock, sf::Text& clock_text);
+    void draw();
 public:
-    Game(unsigned int window_lengh, unsigned int window_height, const std::string& window_name);
+    Game(unsigned int window_width, unsigned int window_height, const std::string& window_name);
     void run();
 };
